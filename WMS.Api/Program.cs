a74c47cc794e62332daae5e106169fa94d39769d
rryszkowski <rryszkowski.reg@gmail.com>
2023-09-27 06:47:25 +0200

    Add projects

--- WMS.Api/Program.cs
+++ WMS.Api/Program.cs
@@ -16,7 +16,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+var summaries = new[] 
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
