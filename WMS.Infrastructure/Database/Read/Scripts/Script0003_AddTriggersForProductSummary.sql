61f50c098eb2278d34a87b796d0b4220aa7daaa9
rryszkowski <rryszkowski.reg@gmail.com>
2023-12-14 19:01:37 +0100

    Add SQL scripts

---
+++ WMS.Infrastructure/Database/Read/Scripts/Script0003_AddTriggersForProductSummary.sql
@@ -0,0 +1,39 @@
+﻿DELIMITER //
+
+CREATE TRIGGER add_new_product
+    AFTER INSERT ON Product
+    FOR EACH ROW 
+BEGIN
+    INSERT INTO ProductSummary (Id, Name, Category, Manufacturer, AvailableQuantity)
+    SELECT NEW.Id, NEW.Name, NEW.Category, NEW.Manufacturer, 0
+    FROM Product P
+    WHERE P.Id = NEW.Id;
+END//
+
+CREATE TRIGGER remove_product_record
+    AFTER DELETE ON Product
+    FOR EACH ROW
+BEGIN
+    DELETE FROM ProductSummary
+    WHERE Id = OLD.Id;
+END//
+
+CREATE TRIGGER update_available_quantity_insert
+    AFTER INSERT ON Inventory
+    FOR EACH ROW
+BEGIN
+    UPDATE ProductSummary
+    SET AvailableQuantity = AvailableQuantity + NEW.Quantity
+    WHERE Id = NEW.ProductId;
+END//
+
+CREATE TRIGGER update_available_quantity_update
+    AFTER UPDATE ON Inventory
+    FOR EACH ROW
+BEGIN
+    UPDATE ProductSummary
+    SET AvailableQuantity = AvailableQuantity + NEW.Quantity - OLD.Quantity
+    WHERE Id = NEW.ProductId;
+END//
+
+DELIMITER ;
---
+++ WMS.Infrastructure/Database/Read/Scripts/Script0004_AddWarehouseSummaryTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE WarehouseSummary (
+    Id CHAR(36) PRIMARY KEY,
+    Location VARCHAR(100),
+    Capacity INT,
+    Utilization INT
+);
