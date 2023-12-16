3e26f1aeaa7d73633b860e28d2a2f31112272cb5
rryszkowski <rryszkowski.reg@gmail.com>
2023-12-16 21:06:54 +0100

    Add OrderView

---
+++ WMS.Infrastructure/Database/Read/Scripts/Script0007_CreateOrderView.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW OrderView AS
+SELECT
+    ROW_NUMBER() OVER () AS OrderViewId,
+    o.Id AS OrderId,
+    o.CustomerId,
+    o.Status,
+    SUM(od.Quantity) AS TotalQuantity,
+    COUNT(od.ProductId) AS TotalProducts
+FROM
+    `Order` o
+JOIN OrderDetails od ON o.Id = od.OrderId
+GROUP BY
+    o.Id, o.CustomerId, o.Status;
