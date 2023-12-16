48e60401d37478043ae08b13415a2871652ae585
rryszkowski <rryszkowski.reg@gmail.com>
2023-12-16 21:24:14 +0100

    Add CreateShipmentView

---
+++ WMS.Infrastructure/Database/Read/Scripts/Script0008_CreateShipmentView.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW ShipmentView AS
+SELECT
+    ROW_NUMBER() OVER () AS ShipmentViewId,
+    s.Id AS ShipmentId,
+    s.OrderId,
+    s.WarehouseId,
+    s.Status,
+    SUM(od.Quantity) AS TotalQuantity
+FROM
+    Shipment s
+JOIN OrderDetails od ON s.OrderId = od.OrderId
+GROUP BY
+    s.Id, s.OrderId, s.WarehouseId, s.Status;
