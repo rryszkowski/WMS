bf9952a19b8f060b470ce89d280a44adc2d2b7a1
rryszkowski <rryszkowski.reg@gmail.com>
2023-12-16 18:44:37 +0100

    Add InventoryView

---
+++ WMS.Infrastructure/Database/Read/Scripts/Script0006_CreateInventoryView.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW InventoryView AS
+SELECT
+    ROW_NUMBER() OVER () AS InventoryViewId,
+    i.ProductId,
+    i.WarehouseId,
+    p.Name AS ProductName,
+    w.Location AS WarehouseLocation,
+    SUM(i.Quantity) AS Quantity
+FROM
+    Inventory i
+JOIN Product p ON i.ProductId = p.Id
+JOIN Warehouse w ON i.WarehouseId = w.Id
+GROUP BY
+    i.ProductId, i.WarehouseId, p.Name, w.Location;
